3461c6179fdb9e1489c4a94248853a1584973668
Ziga201 <ductungpts@gmail.com>
2022-07-21 10:15:48 +0700

    data-tung

---
+++ assets/js/data-sciences.js
@@ -0,0 +1,37 @@
+var swiper =
+        new Swiper('.swiper-container', {
+            slidesPerView: '1',
+            spaceBetween: 0,
+            autoplay: false,
+            loop: true,
+            effect: 'coverflow',
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+                rotate: 0,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+            },
+        }
+        );
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("expertise__tab-item");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
